ef75a0d435662562b319f21a172426c97296042c
Thomas Shamp <T.shamp7@gmail.com>
2020-12-10 10:00:31 -0800

    implement custom hook for useVideos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube.js";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
-class App extends Component {
-  state = {
-    videos: [],
-    selectedVideo: null,
-  };
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("buildings");
 
-  componentDidMount() {
-    this.getSearchTerm("buildings");
-  }
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  getSearchTerm = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
-
-  getSelectedVideo = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar getSearchTerm={this.getSearchTerm} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                getSelectedVideo={this.getSelectedVideo}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar getSearchTerm={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList getSelectedVideo={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class SearchBar extends Component {
-  state = {
-    term: "",
-  };
+const SearchBar = ({ getSearchTerm }) => {
+  const [term, setTerm] = useState("");
 
-  handleChange = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    this.setState({ [e.target.name]: e.target.value });
+    getSearchTerm(term);
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.getSearchTerm(this.state.term);
-
-    // TODO: Add callback to pass input to parent component.
-  };
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.handleSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              name="term"
-              type="text"
-              value={this.state.text}
-              onChange={this.handleChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={handleSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            name="term"
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
